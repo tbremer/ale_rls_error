a84f14f119b470e0dc0a98dfcb9156e7490a84c2
Tom Bremer <tom@tbremer.com>
2018-06-03 12:15:55 -0500

    add comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub fn main() {
-    println!("Hello, world!")
-    println!("Oranges!");
+    println!("Hello, world!"); // removing semicolon here reports error correctly one time, adding to back does not remove the error.
+    println!("Is this an error?");
 }
